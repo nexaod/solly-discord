10cb668a2dbed10071145537047ac8546c9c2d1d
InboxedRS <129743056+InboxedRS@users.noreply.github.com>
2024-06-21 14:48:06 +0100

    Update NecromancyGuide.ts

--- src/NecromancyGuide.ts
+++ src/NecromancyGuide.ts
@@ -16,7 +16,7 @@ client.on("ready", async () => {
     if (!client.user || !client.application) {
         return;
     }
-    const channel = await client.channels.fetch(1253408837906464849) as TextChannel;
+    const channel = await client.channels.fetch('1253408837906464849') as TextChannel;
     await purgeChannel(channel);
     const tocEmbed = new EmbedBuilder()
         .setTitle('Table of Contents')
