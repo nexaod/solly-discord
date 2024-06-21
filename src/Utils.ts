7fff1f9034c8b1ee7860f3104cefcd5e5ae75cf9
InboxedRS <129743056+InboxedRS@users.noreply.github.com>
2024-06-21 12:03:41 +0100

    Update Utils.ts

--- src/Utils.ts
+++ src/Utils.ts
@@ -389,6 +389,8 @@ const ProdChannels = {
     rules: '429380211390545920',
     reaperInfo: '1227389619235061882', //currently set to wip test channel
     reaperTickets: '922046714456715264',
+    necromancyGuide: '1253408837906464849',
+
 }
 
 const DevRoles: any = {
