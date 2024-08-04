79dc5a08a3787db142a295a9fc82ae2b3f138a81
Forest <forest.moses@gmail.com>
2024-08-04 16:47:49 -0700

    Update ReaperInfo Faq added

--- src/data/ReaperInfo.ts
+++ src/data/ReaperInfo.ts
@@ -1,59 +1,73 @@
 import { Channels, Colours, Emojis, styleTitle } from "../../src/Utils";
 
-export const welcome = `Welcome to Solak Discord's **Reaper Info** channel.\n\nOnce you have read through this information please use the channel <#${Channels.reaperTickets}> to create your very own reaper ticket.\n\n\`>\` indicates the preference order of an item, with the last item being the minimum requirement.\n\u200B`;
-
-export const requirements = "For your FREE first Solak kill:\n:bp: You must have your kill count and preset screenshots accepted in your reaper ticket first (See below for exemplars)\n:bp:  While requirements for your reaper kill are relaxed, please comply with one of the exemplar presets below, with T85 weapons and T70 armour as a minimum (1 Defence pures exempt of course). We’ll check your preset again before your kill to ensure it matches the one you posted in your ticket.\n:bp: You MUST watch the guide below before your kill\n:bp: Splits: All Main-hands, Off-hands and Grimoires are split Equally.\nThis is a FREE service we provide, so we ask that you adhere to and respect our advice as much as possible, we're here to help you get the kill and get you closer to reaper crew.";
-
-export const killCountProofPic = 'https://images-ext-1.discordapp.net/external/44salJpVv-pX7HzGuyae4cbwwqY980dUqrUlfGWME1U/https/i.imgur.com/KQtxJJ0.png?format=webp&quality=lossless&width=810&height=562';
+export const welcome = "Welcome to Solak Discord's **Reaper Info** channel.\n\nOnce you have read through this information please use the channel <#${Channels.reaperTickets}> to create your very own reaper ticket.\n\n\`>\` indicates the preference order of an item, with the last item being the minimum requirement.\n\u200BFor your FREE first Solak kill:\n:bp: You must have your kill count and preset screenshots accepted in your reaper ticket first (See below for exemplars)\n:bp:  While requirements for your reaper kill are relaxed, please comply with one of the exemplar presets below, with T85 weapons and T70 armour as a minimum (1 Defence pures exempt of course). We’ll check your preset again before your kill to ensure it matches the one you posted in your ticket.\n:bp: You MUST watch the guide below before your kill\n:bp: Splits: All Main-hands, Off-hands and Grimoires are split Equally.\nThis is a FREE service we provide, so we ask that you adhere to and respect our advice as much as possible, we're here to help you get the kill and get you closer to reaper crew.\n\n\u200B";
 
 export const content = [
     {
+        title: `⬥ __**Killcount Picture:**__`,
+        toc: 'Killcount Picture',
+        colour: Colours.gold,
+        description: `https://images-ext-1.discordapp.net/external/44salJpVv-pX7HzGuyae4cbwwqY980dUqrUlfGWME1U/https/i.imgur.com/KQtxJJ0.png?format=webp&quality=lossless&width=810&height=562`
+    },
+    {
+        title: `⬥ __**General Supplies:**__`,
+        toc: 'Killcount Picture',
+        colour: Colours.gold,
+        description: `Overloads\npowerburst of Vitality\nWeapon Poison+++\nEnhanced Excalibur\nAdrenaline Renewal Potion\nNeceromancy Nexus or Shield\nSaradomin Brews\nSolid Food\nDominion Mines\nDPS Pocket Slot\nVampyrism Aura\nCinderbane Gloves\n`
+    },
+    {
         title: `⬥ __**Melee:**__`,
-        toc: 'Basic',
+        toc: 'Melee',
         colour: Colours.red,
         description: `https://media.discordapp.net/attachments/1261497214841126954/1261498271910596618/image.png?ex=66ad8b51&is=66ac39d1&hm=2d9a29f88b5f7076ed20a93cd297bd72d45a4e358d0fd61b13046d572afafc5c&=&format=webp&quality=lossless&width=322&height=251`
     },
     {
         title: `⬥ __**Ranged:**__`,
-        toc: 'Basic',
+        toc: 'Ranged',
         colour: Colours.green,
         description: `https://media.discordapp.net/attachments/1261497214841126954/1261498368715391057/image.png?ex=66ad8b68&is=66ac39e8&hm=dfc7af9315f55ca151d1f6352d0a4d0ae02c2df6b63abad8e0642be20e10be1f&=&format=webp&quality=lossless&width=323&height=252`
     },
     {
         title: `⬥ __**Magic:**__`,
-        toc: 'Basic',
+        toc: 'Magic',
         colour: Colours.blue,
         description: `https://media.discordapp.net/attachments/1261497214841126954/1261498427683110952/image.png?ex=66ad8b76&is=66ac39f6&hm=60502cbadbd9cd6d5eed7d684757d4570a93d959b404f25ce8b03dbf036bd584&=&format=webp&quality=lossless&width=323&height=252`
     },
     {
         title: `⬥ __**Necromancy**__`,
-        toc: 'Basic',
+        toc: 'Necromancy',
         colour: Colours.purple,
         description: `https://media.discordapp.net/attachments/1261497214841126954/1261498482494279680/image.png?ex=66ad8b83&is=66ac3a03&hm=91810a4793a96f62bf3a08edded63cd1789cfa1f2b2a03ebe0c19a2ea8a0bde1&=&format=webp&quality=lossless&width=320&height=251`
     },
     {
         title: `⬥ __**Familiar:**__`,
-        toc: 'Basic',
+        toc: 'Familiar',
         colour: Colours.gold,
         description: `https://media.discordapp.net/attachments/1261497214841126954/1261504004471394334/image.png?ex=66ad90a8&is=66ac3f28&hm=933eb8efe603785d808210f737774ffbbdfe6d7f2dc60ea9d63e09d09bbbc0ed&=&format=webp&quality=lossless&width=163&height=261`
     },
     {
         title: `⬥ PLEASE DO NOT BRING`,
-        toc: 'Basic',
+        toc: 'Do not bring',
         colour: Colours.gold,
         description: `Blubbers, dps familiars, defenders\nhttps://media.discordapp.net/attachments/1261497214841126954/1261498564350050446/image.png?ex=66ad8b97&is=66ac3a17&hm=d1f22b0fa935afff534edef21dd879e9bb9e61417e892f24356cfc2d0fb35b47&=&format=webp&quality=lossless&width=153&height=27`
     },
     {
         title: `⬥ __**Reaper Guide:**__`,
-        toc: 'Basic',
+        toc: 'Reaper Guide',
         colour: Colours.gold,
-        description: `https://youtu.be/0DCAmWLU70w`
+        description: `Please familiarize youself with the fight's mechanics through this video others:\nEspecially focus on phase 1 mechanics\nhttps://youtu.be/0DCAmWLU70w`
     },
     {
         title: `⬥ __**Guide Disclaimer:**__`,
-        toc: 'Basic',
+        toc: 'Disclaimer',
         colour: Colours.gold,
         description: "The reaper team that takes you will decide the specific strategies you will be using for your kill. Some of the strategies for P2 and P3 in particular are often done differently depending on the team and who takes you. It is quite likely you will not be asked to base for these 2 phases and otherwise will generally be following the rest of the team and following your hosts' instructions. If you are a hardcore, skiller, or new to pvm it is more likely that you will do slower methods like in the video whereas if you have some pvm experience and are looking to learn the boss you may do more strategies of a normal kill"
+    },
+    {
+        title: `⬥ __**FAQ:**__`,
+        toc: 'FAQ',
+        colour: Colours.gold,
+        description: "Q: When will I get my kill?\n\nA: We host our reapers in clusters - our reaper team dedicates an hour or two to getting through as many of your tickets as possible. When we have a time planned, we will (generally) announce a day or two beforehand in ⁠reaper-ticket This will contain the day and time we are hosting the reaper cluster on. Other times our reaper team may contact you in your ticket at their discretion in order to schedule a time with you. Please be patient as all our team are volunteering their time. Also keep in mind we will try not to over-ping you so you may get less impromptu invites if you are showing offline or invisible on discord.\n\nQ: What is my role during the kill?\n\nA: Your job is to simply follow our instructions and stay alive.  Remember: Survive > Damage\n\nQ: Do I have to use a pack yak? A ripper demon would be a lot more damage.\n\nA: Your job is to stay alive so we can get to everyone who is attending the cluster. There are a few places during the kill you will be holding Solak's aggression, so a pack yak is 100% necessary.\n\nQ: I've had a look at your ⁠requirements page, but I don't meet everything on the list. Am I still able to get my reaper kill?\n\nA: We are very lenient on the requirements for your first kill. The ⁠requirements page is for actual team forming, where basic requirements are specific for casual teams and to begin participating in learner hours, while advanced requirements are team-specific and are more applicable for experienced teams doing more challenging strategies.\n\nQ: My DPS isn't the best, will this be an issue?\n\nA: Every member of our reaper team has proven themselves highly capable of hitting DPS checks in any team size across a wide spectrum of experience, so you're in excellent hands. They'll do most of the heavy lifting  Just do your best and most importantly survive and you'll be fine.\n\nQ: The strategy in the reaper guide is very different to how I've seen people kill Solak on streams, why can't we just do a kill like normal?\n\nA: The strategy we use for reaper kills is highly customized for your first kill. We have evolved this strategy over hundreds of kills to give you the best chance of first-attempt success. We have a queue of people to get through in a single cluster, so the strategy best ensures we can see everyone in the allocated time. If you're concerned about not participating enough, don't worry as our strategy sees you taking both base and dps roles, as well as dealing with many group mechanics by yourself all while taking little damage and having fun (which is the main part you should focus on ).\n\nQ: Can i use tank armour such as Cryptbloom when trying to obtain the reaper kill?\n\nA: Yes you can use any tank armour you prefer, your job is to ensure you stay alive so we want you to feel as comfortable as possible.\n\nQ: Can I use necromancy?\n\nA: Yes you can use necromancy to get your first Solak kill! We Have updated our necromancy loadout\n\n"
     },
 ]
 
@@ -86,5 +100,3 @@ export const content = [
 // export const reaper_guide_vid = 'https://youtu.be/0DCAmWLU70w';
 
 // export const vid_disclaimer = "The reaper team that takes you will decide the specific strategies you will be using for your kill. Some of the strategies for P2 and P3 in particular are often done differently depending on the team and who takes you. It is quite likely you will not be asked to base for these 2 phases and otherwise will generally be following the rest of the team and following your hosts' instructions. If you are a hardcore, skiller, or new to pvm it is more likely that you will do slower methods like in the video whereas if you have some pvm experience and are looking to learn the boss you may do more strategies of a normal kill";
-
-export const faq = 'FAQ';
